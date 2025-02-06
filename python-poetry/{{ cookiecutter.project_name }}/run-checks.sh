8107da666c8a06f4a44ae072d52d96f35c6f8ece
janconj <126484070+janconj@users.noreply.github.com>
2025-02-06 18:12:03 +0000

    UPdate run-checks.sh

--- python-poetry/{{ cookiecutter.project_name }}/run-checks.sh
+++ python-poetry/{{ cookiecutter.project_name }}/run-checks.sh
@@ -3,17 +3,17 @@ set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-echo -e "\n== Running pytest =="
+echo -e "\n== Running pytest (tests) =="
 poetry run pytest .
 
-echo -e "\n== Running pyright =="
+echo -e "\n== Running pyright (type check) =="
 poetry run pyright .
 
-echo -e "\n== Running ruff check =="
+echo -e "\n== Running ruff check (linter) =="
 poetry run ruff check .
 
-echo -e "\n== Running ruff format =="
+echo -e "\n== Running ruff format (code formatter) =="
 poetry run ruff format .
 
-echo -e "\n== Running bandit =="
+echo -e "\n== Running bandit (static analyzer) =="
 poetry run bandit -c pyproject.toml -r .

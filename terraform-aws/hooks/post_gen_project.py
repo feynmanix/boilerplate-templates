286e405b55fb466ec4465a7c6360760a54c5080e
Feynmanix <3536362+feynmanix@users.noreply.github.com>
2025-09-20 23:09:45 +0100

    Configurable terraform state bucket suffix (#3)
    * Update main.tf
    
    * Update cookiecutter.json
    
    * Update main.tf
    
    * Update post_gen_project.py

--- terraform-aws/hooks/post_gen_project.py
+++ terraform-aws/hooks/post_gen_project.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 environment_accounts_ids = {{ cookiecutter.environment_aws_account_ids }}
 for environment, account_id in environment_accounts_ids.items():
     Path('env-config').mkdir(parents=True, exist_ok=True)
-    Path(f'env-config/{environment}.aws.tfbackend').write_text(f'bucket = "{account_id}-terraform-tfstate-bucket"\nregion = "{{ cookiecutter.aws_default_region }}"')
+    Path(f'env-config/{environment}.aws.tfbackend').write_text(f'bucket = "{account_id}-terraform-tfstate-bucket{{ cookiecutter.state_bucket_suffix}}"\nregion = "{{ cookiecutter.aws_default_region }}"')
     Path(f'env-config/{environment}.tfvars').write_text(f'env_name = "{environment}"\naws_account_id = "{account_id}"')
     Path('bootstrap/env-config').mkdir(parents=True, exist_ok=True)
     Path(f'bootstrap/env-config/{environment}.tfvars').write_text(f'aws_account_id = "{account_id}"')
